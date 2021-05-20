6351e088328203b7a787bc15a24ff86b11580026
Miguel Nogueira <miguel.nogueira@ua.pt>
2021-05-20 09:54:10 +0100

    validateAcc()

--- app/src/main/java/com/example/app/activities/LoginActivity.java
+++ app/src/main/java/com/example/app/activities/LoginActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.app.R;
 
@@ -34,8 +35,14 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                boolean a = validateAcc(email.getText().toString(), password.getText().toString());
+                if (a) {
+                    Toast.makeText(LoginActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(LoginActivity.this, "Incorrect credentials! Try again", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -47,5 +54,11 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public boolean validateAcc(String email, String password){
+        if(email.equals("admin") && password.equals("admin")){
+            return true;
+        }
+        return false;
     }
 }
