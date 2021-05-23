c3339bcff3035a9f5c4b508ef851ef41337de27b
AnaLu602 <73102402+AnaLu602@users.noreply.github.com>
2021-05-23 16:01:59 +0100

    showing data on today page

--- app/src/main/java/com/example/app/fragments/Today.java
+++ app/src/main/java/com/example/app/fragments/Today.java
@@ -1,12 +1,15 @@
 package com.example.app.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +17,19 @@ import android.view.ViewGroup;
 import com.example.app.App;
 import com.example.app.R;
 import com.example.app.classesAna.MedicationTask;
-import com.example.app.classesAna.Task;
+import com.example.app.models.Task;
 import com.example.app.adapters.TasksRecViewAdapter;
+import com.example.app.models.TaskActivity;
+import com.example.app.models.TaskMeasurement;
+import com.example.app.models.TaskMedication;
+import com.example.app.models.TaskSymptomCheck;
+import com.example.app.models.Treatment;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,30 +38,104 @@ import java.util.ArrayList;
  */
 public class Today extends Fragment {
 
+    private View view;
     private RecyclerView tasksRecView;
+    private TasksRecViewAdapter adapter;
+    private ArrayList<com.example.app.classesAna.Task> todayTasksShow = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         getActivity().setTitle(R.string.nav_today);
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_today, container, false);
-
-        ArrayList<Task> tasks = new ArrayList<>();
-
-        tasks.add(new MedicationTask("Medication","Take pills", "15/05/2021","10:30", "Ben-u-ron", 1));
-        tasks.add(new Task("Measurement","Measure blood pressure","15/05/2021", "15:20"));
-        tasks.add(new Task("Activity","Running","15/05/2021", "18:00"));
-        tasks.add(new Task("Symptom Check","Symptom Check","15/05/2021", "20:00"));
-
-        tasksRecView = view.findViewById(R.id.tasksTodayRecyclerView);
-
-        TasksRecViewAdapter adapter = new TasksRecViewAdapter(view.getContext());
-        adapter.setTasks(tasks);
-
-        tasksRecView.setAdapter(adapter);
-        tasksRecView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        view = inflater.inflate(R.layout.fragment_today, container, false);
 
         return view;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        tasksRecView = view.findViewById(R.id.tasksTodayRecyclerView);
+        setTodayTasksRecycler();
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setTodayTasksRecycler() {
+        Map<LocalTime, Task> todayTasks;
+
+        List<Treatment> treatments = App.listTreatment;
+
+        for (Treatment treatment :
+                treatments) {
+            todayTasks = treatment.getDailyTaskByDate(LocalDate.now());
+            if (todayTasks != null) {
+                treatmentHandlerPicker(treatment, todayTasks);
+            }
+        }
+
+        adapter = new TasksRecViewAdapter(view.getContext());
+        adapter.setTasks(todayTasksShow);
+        tasksRecView.setAdapter(adapter);
+        tasksRecView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void treatmentHandlerPicker(Treatment treatment, Map<LocalTime, Task> dailyTasks) {
+        if (treatment.getType().equals(TaskMedication.class)) {
+            handleMedicationTask(dailyTasks);
+        } else if (treatment.getType().equals(TaskActivity.class)) {
+            handleActivityTask(dailyTasks);
+        } else if (treatment.getType().equals(TaskMeasurement.class)) {
+            handleMeasurementTask(dailyTasks);
+        } else {
+            handleSymptomCheckTask(dailyTasks);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void handleSymptomCheckTask(Map<LocalTime, Task> dailyTasks) {
+
+        for (LocalTime time :
+                dailyTasks.keySet()) {
+
+            TaskSymptomCheck task = (TaskSymptomCheck) dailyTasks.get(time);
+            todayTasksShow.add(new com.example.app.classesAna.Task("Symptom Check", task.getName(), LocalDate.now().toString(), time.toString()));
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void handleMeasurementTask(Map<LocalTime, Task> dailyTasks) {
+
+        for (LocalTime time :
+                dailyTasks.keySet()) {
+
+            TaskMeasurement task = (TaskMeasurement) dailyTasks.get(time);
+            todayTasksShow.add(new com.example.app.classesAna.Task("Measurement", task.getName(), LocalDate.now().toString(), time.toString()));
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void handleActivityTask(Map<LocalTime, Task> dailyTasks) {
+
+        for (LocalTime time :
+                dailyTasks.keySet()) {
+
+            TaskActivity task = (TaskActivity) dailyTasks.get(time);
+            todayTasksShow.add(new com.example.app.classesAna.Task("Activity", task.getName(), LocalDate.now().toString(), time.toString()));
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void handleMedicationTask(Map<LocalTime, Task> dailyTasks) {
+
+        for (LocalTime time :
+                dailyTasks.keySet()) {
+
+            TaskMedication task = (TaskMedication) dailyTasks.get(time);
+            todayTasksShow.add(new MedicationTask("Medication", task.getName(), LocalDate.now().toString(), time.toString(), task.getName(), task.getDose()));
+        }
     }
 }
